e6660433fbbf531afcbd453f657d2ca6d06f8609
aashishogale <aashishogale9@gmail.com>
2018-01-23 19:28:56 +0530

    todoapp started

---
+++ ToDoApp/Todo/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Notes(models.Model):
+    owner=models.ForeignKey(User,on_delete=models.CASCADE)
+    title=models.CharField(max_length=None)
+    description=models.CharField(max_length=None)
+    date_created=models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return 
+
+    def __unicode__(self):
+        return 
+
